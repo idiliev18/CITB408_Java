b282230a708ddb843860ad52633b788756c780ff
Iliyan D. Iliev <idiliev@technologica.com>
2025-02-27 11:10:26 +0200

    Add exercise week 1

---
+++ Semester/week1/exercise/Main.java
@@ -0,0 +1,14 @@
+package week1.exercise;
+
+import week1.exercise.videos.Video;
+
+public class Main {
+    public static void main(String[] args) {
+        Video video = new Video("GALENAXX01","Galena - Tok po tyaloto, 2024", 3.06,34000, false);
+        Video video2 = new Video("GALENAXX02", "Galena - Igrai, 2024", 3.19);
+        Video video3 = new Video("aefhajkhjkas");
+        System.out.println(video);
+        System.out.println(video2);
+        System.out.println(video3);
+    }
+}
--- Semester/week1/exercise/MyFirstJavaClass.java
+++
@@ -1,7 +0,0 @@
-package week1.exercise;
-
-public class MyFirstJavaClass {
-    public static void main(String[] args) {
-        System.out.println("Preslava");
-    }
-}
